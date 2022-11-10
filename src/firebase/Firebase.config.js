24807213e4fca95cedc35bcf060d22bc71ae989a
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-10 07:42:54 -0800

    signup with firebase successfully

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC_wAgs2DifJZdbCT9Es22--UoBDEjQmvg",
@@ -12,7 +11,6 @@ const firebaseConfig = {
   messagingSenderId: "893595820891",
   appId: "1:893595820891:web:755b6b2cb5bac469dd707e"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
